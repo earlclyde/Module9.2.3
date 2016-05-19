d03e2e04a1831297f527e41acdf6e0fa031cc323
Mason Earl <masonc.earl@gmail.com>
2016-05-19 10:08:08 -0600

    Stretch, returning all my people as nil

--- FailableInit2.playground/Contents.swift
+++ FailableInit2.playground/Contents.swift
@@ -2,35 +2,34 @@
 
 import Cocoa
 
-var str = "Hello, playground"
-
-import Cocoa
-
-let workingDictionary = ["nameKey" : "Derek", "ageKey": 28, "favMovieKey" : "Zoolander"]
-let brokenDictionary = ["nameKey" : "Steve", "ageKey": 2]
+let workingDictionary = ["nameKey": "Derek", "ageKey": 28, "favMovieKey": "Zoolander"]
+let brokenDictionary = ["nameKey": "Steve", "ageKey": 2]
 
 class Person {
-    private let nameKey = "name"
-    private let ageKey = "age"
-    private let favoriteMovieKey = "favoriteMovie"
+    let nKey = "nameKey"
+    let aKey = "ageKey"
+    let fKey = "favoriteMovieKey"
     
-    var name: String = ""
-    var age: Int = 0
-    var favoriteMovie: String = ""
+    var name: String?
+    var age: Int?
+    var favoriteMovie: String?
     
-    init(dictionary: [String: AnyObject]){
-        
+    init?(dictionary: [String:AnyObject]) {
+        guard let name = dictionary[nKey] as? String,
+                  age = dictionary[aKey] as? Int,
+                  favoriteMovie = dictionary[fKey] as? String else {
+                        return nil
+                }
+                self.name = name
+                self.age = age
+                self.favoriteMovie = favoriteMovie
     }
     
-    init(name: String, age: Int, favoriteMovie: String) {
-        self.name = name
-        self.age = age
-        self.favoriteMovie = favoriteMovie
-        
-        
-        let personDictionary: [String: AnyObject] = [nameKey: "name", ageKey: "age", favoriteMovieKey: "favoriteMovie"]
-        
-    }
 }
+
+let personOne = Person(dictionary: workingDictionary)
+let personTwo = Person(dictionary: brokenDictionary)
+let personThree = Person(dictionary: ["nameKey":"Ben", "ageKey":33, "favortieMovieKey":"Cat Battles"])
+
 
 
